8653b4dcbe5acaed06ae087a18502ae9995cee76
DamianyRosales <damiany.rosales.is@unipolidgo.edu.mx>
2022-05-23 12:17:49 -0500

    booking app

---
+++ booking-app/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+
+	var conferenceName string = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets int = conferenceTickets
+	var bookings []string
+
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+
+	fmt.Printf("Hi, welcome to %v booking application \n", conferenceName)
+	fmt.Println("We have total of ", conferenceTickets, "tickets and", remainingTickets, "are still available")
+
+	fmt.Println("Book your tickets in here:")
+
+	fmt.Println(conferenceName)
+
+	for remainingTickets > 0 && len(bookings) < 50 {
+
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets int
+
+		// ask user for their name
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
+
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
+
+		fmt.Println("Enter your email address: ")
+		fmt.Scan(&email)
+
+		fmt.Println("How many tickets you want?  ")
+		fmt.Scan(&userTickets)
+
+		isValidName := len(firstName) >= 2 && len(lastName) => 2
+		isValidEmail := strings.Contains(email, "@")
+
+		if userTickets <= remainingTickets {
+			remainingTickets = remainingTickets - userTickets
+			bookings = append(bookings, firstName+" "+lastName)
+
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+			fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+			firstNames := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+
+			}
+			fmt.Printf("The first names of bookings are: %v\n", firstNames)
+
+			if remainingTickets == 0 {
+				// end program
+				fmt.Println("Our conferece is booked out. Come back next year.")
+				break
+			}
+		} else {
+			fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets\n", remainingTickets, userTickets)
+			continue
+		}
+
+	}
+}
