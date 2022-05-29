ccd9207f455a75b8bd2c042d9847438b9489e33d
DamianyRosales <damiany.rosales.is@unipolidgo.edu.mx>
2022-05-29 01:58:40 -0500

    booking app user validation

--- booking-app/main.go
+++ booking-app/main.go
@@ -41,10 +41,11 @@ func main() {
 		fmt.Println("How many tickets you want?  ")
 		fmt.Scan(&userTickets)
 
-		isValidName := len(firstName) >= 2 && len(lastName) => 2
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
 		isValidEmail := strings.Contains(email, "@")
+		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
 
-		if userTickets <= remainingTickets {
+		if isValidName && isValidEmail && isValidTicketNumber {
 			remainingTickets = remainingTickets - userTickets
 			bookings = append(bookings, firstName+" "+lastName)
 
@@ -65,7 +66,15 @@ func main() {
 				break
 			}
 		} else {
-			fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets\n", remainingTickets, userTickets)
+			if !isValidName {
+				fmt.Println("The name you entered is too short")
+			}
+			if !isValidEmail {
+				fmt.Println("The Email address you entered does not contain @ sign")
+			}
+			if !isValidTicketNumber {
+				fmt.Println("Number of tickets you entered is invalid")
+			}
 			continue
 		}
 
